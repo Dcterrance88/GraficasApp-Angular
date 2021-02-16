48192f88e26e1ae74d6b3bdca5a0ac584b9e74a9
Dcterrance88 <>
2021-02-16 11:13:01 -0500

    Grafica dona
    se realizo en componente dona un ejemplo basico de como hacer una
    grafica de donna y en donna http por medio de un servicio se obtiene la
    informacion y se le da a las propiedades de la grafica de donna para que
    la pueda mostrar.

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona-http',
@@ -8,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others'
+  ];
+  public doughnutChartData: MultiDataSet = [
+    // [350, 450, 100, 150],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#66FFFF',
+        '#5DE8C1',
+        '#74FFB0',
+        '#5DE872',
+        '#7FFF66',
+      ],
+    }
+  ]
+
+  constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
+    // this.graficasService.getUsuariosRedesSociales()
+    //   .subscribe( data => {
+    //     const labels = Object.keys(data);
+    //     const values = Object.values(data);
+        
+    //     this.doughnutChartLabels = labels;
+    //     this.doughnutChartData.push(values);
+    //   })
+
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+      .subscribe( ({labels, values}) => {
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.push(values);
+      })
   }
 
 }
--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others']; 
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100, 150],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#66FFFF',
+        '#5DE8C1',
+        '#74FFB0',
+        '#5DE872',
+        '#7FFF66',
+      ],
+    }
+  ]
 
 }
